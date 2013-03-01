a4b1541fbdde371a8741c75198f98a0aa9a48125
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2013-03-01 17:54:46 +0100

    Update version by change to python_spain repo

--- symposion/sponsorship/urls.py
+++ symposion/sponsorship/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
+#from django.views.generic.simple import direct_to_template
+#Changes 1.5
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns("symposion.sponsorship.views",
-    url(r"^$", direct_to_template, {"template": "sponsorship/list.html"}, name="sponsor_list"),
+    #url(r"^$", direct_to_template, {"template": "sponsorship/list.html"}, name="sponsor_list"),
+    url(r"^$", TemplateView.as_view(template_name="sponsorship/list.html")),
     url(r"^apply/$", "sponsor_apply", name="sponsor_apply"),
     url(r"^add/$", "sponsor_add", name="sponsor_add"),
     url(r"^(?P<pk>\d+)/$", "sponsor_detail", name="sponsor_detail"),
